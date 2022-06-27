f0762b0c27d102c5c2d00777732905a18844721a
jainendrasnowflake <108261173+jainendrasnowflake@users.noreply.github.com>
2022-06-27 04:47:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,11 @@ streamlit.title('(: My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu');
 streamlit.text('Bread, Butter');
 streamlit.text('Milk, Sugar');
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries']);
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
 streamlit.dataframe(my_fruit_list);
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index));
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries']);
