fb3f0dbb96da1fe742a89a53995ef58e7fcc231a
jainendrasnowflake <108261173+jainendrasnowflake@users.noreply.github.com>
2022-06-27 04:29:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit;
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('(: My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu');
 streamlit.text('Bread, Butter');
 streamlit.text('Milk, Sugar');
