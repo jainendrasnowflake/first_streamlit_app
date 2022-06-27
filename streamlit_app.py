589f3adb3d050cc5d2f5a2ba3f6e433507c8754c
jainendrasnowflake <108261173+jainendrasnowflake@users.noreply.github.com>
2022-06-27 05:20:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.header('Breakfast Menu');
 streamlit.text('Bread, Butter');
 streamlit.text('Milk, Sugar');
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+
 fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),[1,2,3])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
